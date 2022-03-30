d99ae5f4e1f6a7127dbf5cba3cbadada543191b9
+ <+>
2022-03-30 18:31:18 -0400

    commit tree 9c1a04958e7c308196002b3f1adf34cb7f9f1e52

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,15 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
+import { useReducer } from "react/cjs/react.production.min";
 console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} color={user.color} city={user.city}/>
+      <About bio={user.bio} />
     </div>
   );
 }
